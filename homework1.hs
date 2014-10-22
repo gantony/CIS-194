a135f9216c8ba99cb79890bde802d23a58ab595a
Antony <antony.guinard@gmail.com>
2014-10-22 15:40:09 +0100

    Added HUnit tests to homework1

--- homework1.hs
+++ homework1.hs
@@ -1,6 +1,8 @@
 
 -- Instructions: http://www.seas.upenn.edu/~cis194/spring13/hw/01-intro.pdf
 
+import Test.HUnit
+
 
 -- Exercise 1
 
@@ -28,3 +30,26 @@ multiplyPair (n, m) = n*m
 
 doubleEveryOther :: [Integer] -> [Integer]
 doubleEveryOther list = map multiplyPair (zip list (multiplierList list))
+
+
+-- Tests
+
+testCharToString = TestCase $ assertEqual 
+    "return String representation of given Char" "1" (charToString '1')
+
+testCharToInt = TestCase $ assertEqual 
+    "return Integer representation of given Char" 1 (charToInt '1')
+
+testToDigits = TestList [
+    TestCase $ assertEqual 
+        "return list of individual Integers representating a given number" [1,2,3,4] (toDigits 1234),
+    TestCase $ assertEqual 
+        "return [] if 0" [] (toDigits 0),
+    TestCase $ assertEqual 
+        "return [] if <0" [] (toDigits (-17))
+    ]
+
+ex1Tests = TestList [testCharToString, testCharToInt, testToDigits] 
+
+
+main = runTestTT $ TestList [ex1Tests]
