6b43fea6f719a7b69cea57961ac1f0bbf3dc2b07
Antony Guinard <antony.guinard@gmail.com>
2014-11-18 08:54:28 +0000

    Added week3 ex2

--- week3/Golf.hs
+++ week3/Golf.hs
@@ -13,3 +13,23 @@ keepEvery list n = foldl (\ acc (i, elm) -> if (elm `mod` n) /= 0 then acc else 
 
 skips :: [a] -> [[a]]
 skips list = map (\ (_,x) -> keepEvery list x ) (zip list [1..])
+
+
+-- Exercise 2
+
+data Triplet = Triplet Integer Integer Integer
+             deriving Show
+
+triplets :: [Integer] -> [Triplet]
+triplets (x:y:z:zs) = [Triplet x y z] ++ triplets (y:z:zs)
+triplets _ = []
+
+single :: Triplet -> Integer
+single (Triplet _ x _) = x
+
+isMaxima :: Triplet -> Bool
+isMaxima (Triplet a b c) = b > a && b > c
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima list = map single $ filter isMaxima (triplets list)
+
