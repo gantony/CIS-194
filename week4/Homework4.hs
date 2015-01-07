bf05edf49996c293822c78d62ec5c7f1df6aad6a
Antony Guinard <antony.guinard@gmail.com>
2015-01-07 09:06:49 +0000

    Exercise 3 week 4

--- week4/Homework4.hs
+++ week4/Homework4.hs
@@ -17,3 +17,29 @@ fun1' list = foldr (*) 1 . map (subtract 2) $ filter even list
 
 -- import Test.QuickCheck
 -- quickCheck (\xs -> fun1 xs == fun1' xs)
+
+-- Come back to fun2 later
+
+
+-- Exercise 2
+
+-- Started on another computer I think
+
+
+-- Exercise 3
+
+xor :: [Bool] -> Bool
+xor = foldr (\ x acc -> if x then not acc else acc) False   -- Partial function application
+
+-- xor [False, True, False] == True
+-- xor [False, True, False, False, True] == False
+
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr (\ x xs -> (f x):xs) []
+
+map'' :: (a -> b) -> [a] -> [b]
+map'' f = foldr ((:) . f) []    -- Same as map', but the result of f is fed as an input to (:)!
+
+map''' :: (a -> b) -> [a] -> [b]
+map''' f = foldr (\ x -> ((f x) : ) ) []    -- This works too, and could be an intermediary step to get to map'' from map', using currying
+
