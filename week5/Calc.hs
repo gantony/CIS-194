81fb9f5d385f05b296e579df2c5c76c36cfc788d
Antony Guinard <antony.guinard@gmail.com>
2015-03-07 22:55:23 +0000

    Week 5 exercise 2

--- week5/Calc.hs
+++ week5/Calc.hs
@@ -2,9 +2,16 @@
 module Calc where
 
 import ExprT
+import Parser
 
 
 eval :: ExprT -> Integer
 eval (Lit x) = x
 eval (Add a b) = eval a + eval b
 eval (Mul a b) = eval a * eval b
+
+evalStr :: String -> Maybe Integer
+evalStr string = case parseExp Lit Add Mul string of
+	Just expression -> Just (eval expression)
+	Nothing -> Nothing
+
