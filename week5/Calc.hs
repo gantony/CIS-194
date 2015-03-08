28078da7343228a34d3ceac20489cbc37a029059
Antony Guinard <antony.guinard@gmail.com>
2015-03-08 00:01:18 +0000

    Week 5 exercise 4

--- week5/Calc.hs
+++ week5/Calc.hs
@@ -27,4 +27,37 @@ class Expr a where
 instance Expr ExprT where
 	lit x = (Lit x)
 	add a b = (Add a b)
-	mul a b =  (Mul a b)
+	mul a b = (Mul a b)
+
+instance Expr Integer where
+	lit x = x
+	add a b = a + b
+	mul a b = a * b 
+
+instance Expr Bool where
+	lit x
+		| x > 0 = True
+		| otherwise = False
+	add a b = a || b
+	mul a b = a && b 
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+
+instance Expr MinMax where
+	lit x = (MinMax x)
+	add (MinMax a) (MinMax b) = MinMax (max a b)
+	mul (MinMax a) (MinMax b) = MinMax (min a b) 
+
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+-- Preventing future duplication :)
+mod7 :: Integer -> Mod7
+mod7 = Mod7 . mod 7 	-- same as mod7 x = Mod7 (mod 7 x)
+
+instance Expr Mod7 where	
+	lit x = mod7 x
+	add (Mod7 a) (Mod7 b) = mod7 $ a + b
+	mul (Mod7 a) (Mod7 b) = mod7 $ a * b 
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
