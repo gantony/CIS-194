a85ea9bb8370c033f9bb86f845c7b82d34f6cd00
Antony Guinard <antony.guinard@gmail.com>
2015-12-09 21:58:35 +0000

    Exercises 3 and 4 week 6

--- week6/homework6.hs
+++ week6/homework6.hs
@@ -15,3 +15,23 @@ fibs1 = map fib [0..]
 -- http://zvon.org/other/haskell/Outputprelude/scanl_f.html
 fibs2 :: [Integer] 
 fibs2 = 0 : scanl (+) 1 fibs2
+
+
+data Stream a = Cons a (Stream a)  -- deriving (Show)
+
+streamToList :: Stream a -> [a]
+streamToList (Cons x xs) = x : streamToList xs
+
+
+instance Show a => Show (Stream a) where
+	show (Cons x xs) = Prelude.show . take 20 $ streamToList (Cons x xs)
+
+
+streamRepeat :: a -> Stream a
+streamRepeat x = (Cons x (streamRepeat x))
+
+streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap f (Cons x xs) = (Cons (f x) (streamMap f xs))
+
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed f x = (Cons x (streamFromSeed f (f x)))
